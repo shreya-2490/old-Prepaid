0ea1bec9e00ec1fe4ab37a390c1b8105a2a42a1a
shreya072001 <gsmile.india201@gmail.com>
2023-06-20 00:49:03 +0530

    mobile-case

--- src/navbar.js
+++ src/navbar.js
@@ -2,12 +2,17 @@ import React, { useState } from "react"
 import "./navbar.css"
 import { Link } from "react-router-dom"
 import logo from "./assets/logo.png"
+import ham from "./assets/ham.png"
 import {UserOutlined} from '@ant-design/icons';
 function Navbar() {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartCount, setCartCount] = useState(0)
   const [resmenu, setresmenu] = useState("none")
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const handleCartClick = () => {
     setIsCartOpen(true)
   }
@@ -64,6 +69,21 @@ function Navbar() {
         <img src={logo}></img>
       </div>
       <div className="left">
+        <div
+          className="hamburg"
+          onClick={() => {
+            if (resmenu == "none") setresmenu("flex");
+            else setresmenu("none");
+          }}
+        >
+          <img src={ham}></img>
+          <div className="navmenu" style={{ display: resmenu }}>
+          <Link to="/">HOME</Link>
+        <Link to="/bulkorder">BULK ORDERS</Link>
+        <Link to="/contactus">CONTACT US</Link>
+        <Link to="/login"><span className="user"><UserOutlined />LOGIN</span></Link>
+          </div>
+        </div>
         <Link to="/">HOME</Link>
         <Link to="/bulkorder">BULK ORDERS</Link>
         <Link to="/contactus">CONTACT US</Link>
